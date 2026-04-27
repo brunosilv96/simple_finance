6dd1c59683ca1d9dd4bb9174dc24c8789becb6e4
Bruno Silva <bruno.silv96@hotmail.com>
2026-04-27 17:48:11 -0300

    feat: simple reatribution

--- internal/http/router/user_routes.go
+++ internal/http/router/user_routes.go
@@ -4,6 +4,8 @@ import "github.com/gin-gonic/gin"
 
 func registerUserRoutes(v1 *gin.RouterGroup, h UserHandler) {
 	users := v1.Group("/users")
-	users.POST("", h.Create)
-	users.GET("/:id", h.FindByID)
+	{
+		users.POST("", h.Create)
+		users.GET("/:id", h.FindByID)
+	}
 }
